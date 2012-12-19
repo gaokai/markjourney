be2858731f0e85e7ba0ee958b59d025083cff0e5
lorrylockie <lorrylockie@gmail.com>
2012-12-20 00:46:37 +0800

    clean some laji code

--- assets/www/javascript/mark-jour.js
+++ assets/www/javascript/mark-jour.js
@@ -1,3 +1,10 @@
+/**
+ * @fileOverview 负责旅途的"记录"部分的功能
+ *               主要包括: 开始旅途，输入旅途名称，旅途过程中的拍照，录音，文字以及相应的数据存储。
+ * @author shiba<shiba@taobao.com>
+ *
+ */
+;
 (function () {
     var markJour = (function () {
         var map;
@@ -8,32 +15,35 @@
          */
         function initMap() {
             return map = new google.maps.Map(document.getElementById('map_canvas'), {
-                zoom:15,
-                mapTypeId:google.maps.MapTypeId.ROADMAP,
-                panControl:false,
-                zoomControl:true,
-                mapTypeControl:false,
-                scaleControl:false,
-                streetViewControl:false,
-                overviewMapControl:false
+                center:new google.maps.LatLng(30, 120),
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                panControl: false,
+                zoomControl: true,
+                mapTypeControl: false,
+                scaleControl: false,
+                streetViewControl: false,
+                overviewMapControl: false
             });
         }
 
         function initOptEvent() {
-            $('#start-go').click(function(e){
+            $('#start-go').click(function (e) {
 
             });
         }
 
 
         return {
-            init:function () {
+            init: function () {
+                console.log(1122);
                 initMap();
                 initOptEvent();
             }
         }
     })();
 
-    markJour.init();
+    window.MJ = window.MJ || {};
+    window.MJ.markJour = markJour;
 
 })();
